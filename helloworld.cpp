239e61cf570bdd67a7904873a4c963525be9a834
leonard <xiaoyiwu6@gmail.com>
2021-01-20 11:25:37 +0800

    C++for特性注释

--- helloworld.cpp
+++ helloworld.cpp
@@ -9,6 +9,8 @@ int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
+    //使用&直接赋值地址，对原始数据进行修改
+    //不用&赋值对应的值，创建副本，不对原始数据修改
     for (const string& word : msg)
     {
         cout << word << " ";
